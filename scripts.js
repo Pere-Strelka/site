384ade7eaa34285f403068a2d72824bf5a7360e5
Pere_Strelka <pere_strelka@mail.ru>
2021-10-18 01:47:58 +0500

    Refactoring

--- scripts.js
+++ scripts.js
@@ -5,17 +5,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function getOffset(el) {
-    var _x = 0;
-    var _y = 0;
-    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
-
 async function copyToClipboard(str, parent_el_id) {
 
     navigator.clipboard.writeText(str);
@@ -28,11 +17,6 @@ async function copyToClipboard(str, parent_el_id) {
 
     await sleep(2000);
     document.getElementById(parent_el_id).removeChild(text);
-}
-
-function makeNavProperties(root, el_name) {
-    root.style.setProperty('--' + el_name + '-offset', getOffset(document.getElementById(el_name)).left + 'px');
-    root.style.setProperty('--' + el_name + '-width', document.getElementById(el_name).offsetWidth + 'px');
 }
 
 function getDeviceType() {
@@ -71,9 +55,6 @@ function initProperties() {
     }
 }
 
-window.addEventListener('resize', function (event) {
-    makeNavProperties(root, 'nav-btn-main');
-    makeNavProperties(root, 'nav-btn-portfolio');
-    makeNavProperties(root, 'nav-btn-contacts');
-});
+// window.addEventListener('resize', function (event) {
+// });
 
