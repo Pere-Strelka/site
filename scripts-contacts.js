32dcae180bec9ca0feb2fb7cb3839db3167444d8
Pere_Strelka <pere_strelka@mail.ru>
2021-10-18 19:36:23 +0500

    Patch font-size

--- scripts-contacts.js
+++ scripts-contacts.js
@@ -2,7 +2,7 @@ function initContactsProperties() {
     if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
         root.style.setProperty('--card-size', '23rem');
-        root.style.setProperty('--card-banner-font-size', '3rem');
+        root.style.setProperty('--card-banner-font-size', '4rem');
     } else {
         document.getElementById('cards_div').classList.add("cards-desktop-grid");
         root.style.setProperty('--card-size', '18rem');
