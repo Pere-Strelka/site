b7a08624e45025f66686abdee797bb785d94b458
Pere_Strelka <pere_strelka@mail.ru>
2021-10-21 14:18:51 +0500

    Update contacts

--- scripts-contacts.js
+++ scripts-contacts.js
@@ -2,10 +2,10 @@ function initContactsProperties() {
     if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
         root.style.setProperty('--card-size', '23rem');
-        root.style.setProperty('--card-banner-font-size', '4rem');
+        root.style.setProperty('--card-banner-font-size', '2.25rem');
     } else {
         document.getElementById('cards_div').classList.add("cards-desktop-grid");
         root.style.setProperty('--card-size', '18rem');
-        root.style.setProperty('--card-banner-font-size', '2rem');
+        root.style.setProperty('--card-banner-font-size', '1.2rem');
     }
 }
