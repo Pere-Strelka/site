7a53b216955665e5b4e148a10bb79505d1260d1f
Pere_Strelka <pere_strelka@mail.ru>
2021-10-21 18:19:44 +0500

    Update contacts

--- scripts-contacts.js
+++ scripts-contacts.js
@@ -1,8 +1,16 @@
+function initContacts() {
+    initContactsProperties();
+
+    if (deviceType == 'mobile') {
+        document.getElementById(parent_el_id).classList.add('card-banner-email-text-mobile-eng');
+    }
+}
+
 function initContactsProperties() {
     if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
         root.style.setProperty('--card-size', '23rem');
-        root.style.setProperty('--card-banner-font-size', '2.25rem');
+        root.style.setProperty('--card-banner-font-size', '2rem');
     } else {
         document.getElementById('cards_div').classList.add("cards-desktop-grid");
         root.style.setProperty('--card-size', '18rem');
@@ -20,10 +28,10 @@ async function copyEmailToClipboard(str, parent_el_id) {
     document.getElementById(parent_el_id).appendChild(text);
 
     await sleep(2000);
-    text.classList.add("card-banner-text-copied--delete");
-
     var dur = 250;
     root.style.setProperty('--text-copied-disappearing-duration', dur + 'ms');
+    text.classList.add("card-banner-text-copied--delete");
+    
     await sleep(dur);
     text.remove();
 }
