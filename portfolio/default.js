f487de220b2197039fdcc5f354eba8b2456b7a54
Pere_Strelka <pere_strelka@mail.ru>
2021-10-30 01:13:54 +0500

    Patch to major update

--- portfolio/default.js
+++ portfolio/default.js
@@ -5,11 +5,15 @@ function initPortfolio() {
 function initPortfolioProperties() {
     if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
-        root.style.setProperty('--card-height', '23rem');
-        root.style.setProperty('--card-banner-font-size', '2.25rem');
+        root.style.setProperty('--card-height', '21rem');
+        root.style.setProperty('--card-font-size', '3rem');
+        root.style.setProperty('--card-title-font-size', '5rem');
+        root.style.setProperty('--logo-size', '3.5rem');
     } else {
         document.getElementById('cards_div').classList.add("cards-desktop-grid");
         root.style.setProperty('--card-height', '16rem');
-        root.style.setProperty('--card-banner-font-size', '1.2rem');
+        root.style.setProperty('--card-font-size', '1.1rem');
+        root.style.setProperty('--card-title-font-size', '1.5rem');
+        root.style.setProperty('--card-title-font-size', '1.75rem');
     }
 }
