889e95163de420b392dc074ba9f9b6aa236b7b2c
Pere_Strelka <pere_strelka@mail.ru>
2021-10-31 00:15:20 +0500

    Patching patch Need to back it up

--- common-scripts.js
+++ common-scripts.js
@@ -1,12 +1,35 @@
 var root = document.querySelector(':root');
+const deviceType = getDeviceType();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function getDeviceType() {
+    const userAgent = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgent)) {
+        return "tablet";
+    }
+    if (/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(userAgent)) {
+        return "mobile";
+    }
+    return "desktop";
+};
+
 function initDefaultProperties() {
     var buttons = document.getElementsByClassName('nav-btn');
-    if (navigator.userAgentData.mobile) {
+    
+    if (deviceType == "tablet") {
+        for (index = 0; index < buttons.length; index++) {
+            buttons[index].classList.add('nav-btn-tablet');
+        }
+        root.style.setProperty('--footer-display', 'flex');
+        root.style.setProperty('--header-nickname-width', 'auto');
+        root.style.setProperty('--header-nickname-text-align', 'unset');
+        root.style.setProperty('--nav-btn-text-size', '1.5rem');
+        root.style.setProperty('--header-nickname-size', '3rem');
+        root.style.setProperty('--header-nickname-margin-left', '10%');
+    } else if (deviceType == 'mobile') {
         for (index = 0; index < buttons.length; index++) {
             buttons[index].classList.add('nav-btn-mobile');
         }
--- portfolio/default.js
+++ portfolio/default.js
@@ -3,7 +3,7 @@ function initPortfolio() {
 }
 
 function initPortfolioProperties() {
-    if (navigator.userAgentData.mobile) {
+    if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
         root.style.setProperty('--card-height', '21rem');
         root.style.setProperty('--card-font-size', '2.2rem');
