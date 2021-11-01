232f68c30c6f0293c22f02e9be84b0fd5eeae57a
Pere_Strelka <pere_strelka@mail.ru>
2021-11-01 17:37:55 +0500

    Update + patch

--- common-scripts.js
+++ common-scripts.js
@@ -24,33 +24,27 @@ function initDefaultProperties() {
             buttons[index].classList.add('nav-btn-tablet');
         }
         root.style.setProperty('--footer-display', 'flex');
-        root.style.setProperty('--header-nickname-width', 'auto');
-        root.style.setProperty('--header-nickname-text-align', 'unset');
+        root.style.setProperty('--header-height', '12.5%');
         root.style.setProperty('--nav-btn-text-size', '1.5rem');
         root.style.setProperty('--header-nickname-size', '3rem');
-        root.style.setProperty('--header-nickname-margin-left', '10%');
         root.style.setProperty('--body-width', '94%');
     } else if (deviceType == 'mobile') {
         for (index = 0; index < buttons.length; index++) {
             buttons[index].classList.add('nav-btn-mobile');
         }
         root.style.setProperty('--footer-display', 'none');
-        root.style.setProperty('--header-nickname-width', '100%');
-        root.style.setProperty('--header-nickname-text-align', 'center');
+        root.style.setProperty('--header-height', '10%');
         root.style.setProperty('--nav-btn-text-size', '2.25rem');
-        root.style.setProperty('--header-nickname-size', '4.5rem');
-        root.style.setProperty('--header-nickname-margin-left', 'auto');
+        root.style.setProperty('--header-nickname-size', '4rem');
         root.style.setProperty('--body-width', '80%');
     } else {
         for (index = 0; index < buttons.length; index++) {
             buttons[index].classList.add('nav-btn-desktop');
         }
         root.style.setProperty('--footer-display', 'flex');
-        root.style.setProperty('--header-nickname-width', 'auto');
-        root.style.setProperty('--header-nickname-text-align', 'unset');
+        root.style.setProperty('--header-height', '15%');
         root.style.setProperty('--nav-btn-text-size', '1.1rem');
         root.style.setProperty('--header-nickname-size', '2.5rem');
-        root.style.setProperty('--header-nickname-margin-left', '10%');
         root.style.setProperty('--body-width', '80%');
     }
 }
--- portfolio.js
+++ portfolio.js
@@ -5,9 +5,9 @@ function initPortfolio() {
 function initPortfolioProperties() {
     if (deviceType == 'mobile') {
         document.getElementById('cards_div').classList.add("cards-mobile-grid");
-        root.style.setProperty('--card-height', '21rem');
+        root.style.setProperty('--card-height', '26rem');
         root.style.setProperty('--card-width', '100%');
-        root.style.setProperty('--card-font-size', '2.2rem');
+        root.style.setProperty('--card-font-size', '1.5rem');
         root.style.setProperty('--card-title-font-size', '2.75rem');
         root.style.setProperty('--logo-size', '3.5rem');
         root.style.setProperty('--card-img-display', 'none');
